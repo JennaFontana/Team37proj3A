8aa2d7f59e9d2bc04096707773d845bd25fac2f2
Jenna Fontana <jennafontana@wifi-roaming-128-4-68-183.wifi.udel.edu>
2025-10-14 21:55:07 -0400

    Initial commit

---
+++ src/Team37proj3A.c
@@ -0,0 +1,9 @@
+/***********************************************
+* TEAM 37: J. PARK and J. FONTANA
+* CPEG222 Project1B, 10/3/25
+* NucleoF446RE CMSIS Sequence PMOD SSDs
+***********************************************/
+#include "stm32f4xx.h"
+#include <stdbool.h>
+#include <stdint.h>
+
